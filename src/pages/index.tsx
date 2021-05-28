2a2e3f45d02e0783583f6465079a5aff83c859d9
Thiago Bignotto <bignotto@gmail.com>
2021-05-28 17:50:35 -0300

    Project start

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Lets Travel! Title text</h1>
+      <p>Lets Travel! text</p>
+    </>
+  );
+}
