09b55139d6b191ceaccc615a6df8482e8fe41379
Thiago Bignotto <bignotto@gmail.com>
2021-05-29 07:30:14 -0300

    project configured
    Added ChakraUI and configured theme, colors, fonts and started development

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
+import { Text } from "@chakra-ui/react";
+import { Header } from "../components/Header";
 export default function Home() {
-  return (
-    <>
-      <h1>Lets Travel! Title text</h1>
-      <p>Lets Travel! text</p>
-    </>
-  );
+  return <Header />;
 }
