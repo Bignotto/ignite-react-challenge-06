8154b6b23e0749e508fab7a459c594c005e74906
Thiago Bignotto <bignotto@gmail.com>
2021-05-30 17:38:05 -0300

    fixed layout and category button responsiveness

--- src/components/CategoryButton/index.tsx
+++ src/components/CategoryButton/index.tsx
@@ -22,9 +22,14 @@ export function CategoryButton({
     );
   }
   return (
-    <Flex direction="column" alignItems="center">
-      <Image src="/images/bullet.svg" w="3" display="flex" />
-      <Text mt="6" fontSize="md" fontWeight="bold">
+    <Flex
+      direction="column"
+      alignItems="center"
+      flexDirection="row"
+      justify="center"
+    >
+      <Image src="/images/bullet.svg" w="3" />
+      <Text mt="1" fontSize="sm" fontWeight="bold" px="2">
         {category}
       </Text>
     </Flex>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import {
   useBreakpointValue,
   Spacer,
   Stack,
+  SimpleGrid,
 } from "@chakra-ui/react";
 import { CategoryButton } from "../components/CategoryButton";
 import { Header } from "../components/Header";
@@ -21,17 +22,14 @@ export default function Home() {
     <Flex align="center" direction="column">
       <Header />
       {/* <Image src="/images/Background.png" objectFit="cover" w="100%" h={335} /> */}
-      <Flex justifyContent="center">
-        <HStack spacing="40">
-          <Box bgImage="/images/Background.png">
-            <Text fontSize={36} color="gray.50">
-              5 Continentes, infinitas
-              <br />
-              possibilidades.
+      <Flex justifyContent="center" bgImage="/images/Background.png" w="100%">
+        <HStack spacing="40" height={335}>
+          <Box mx="4" maxWidth={475}>
+            <Text fontSize={[30, 36]} color="gray.50">
+              5 Continentes, infinitas possibilidades.
             </Text>
             <Text fontSize={20} color="gray.50">
-              Chegou a hora de tirar do papel
-              <br />a viagem que você sempre sonhou.
+              Chegou a hora de tirar do papel a viagem que você sempre sonhou.
             </Text>
           </Box>
           {isWide && (
@@ -45,8 +43,14 @@ export default function Home() {
           )}
         </HStack>
       </Flex>
-      <Flex w="100%" maxW={1440} justifyContent="center" mt="20">
-        <Stack direction={isWide ? "row" : "column"} spacing="36">
+      <Flex w="100%" maxW={1440} justifyContent="center" mt={[8, 20]}>
+        <SimpleGrid
+          columns={[2, 5]}
+          spacing={[10, 24]}
+          p="3"
+          w="100%"
+          maxWidth={1000}
+        >
           <CategoryButton
             category="vida noturna"
             image="/images/cocktail.svg"
@@ -68,12 +72,42 @@ export default function Home() {
             isWide={isWide}
           />
           <CategoryButton
-            category="e mais"
+            category="e mais..."
             image="/images/earth.svg"
             isWide={isWide}
           />
-        </Stack>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );
 }
+
+/**
+         <Stack direction={isWide ? "row" : "column"} spacing={[12, 36]}>
+          <CategoryButton
+            category="vida noturna"
+            image="/images/cocktail.svg"
+            isWide={isWide}
+          />
+          <CategoryButton
+            category="praia"
+            image="/images/surf.svg"
+            isWide={isWide}
+          />
+          <CategoryButton
+            category="moderno"
+            image="/images/building.svg"
+            isWide={isWide}
+          />
+          <CategoryButton
+            category="clássico"
+            image="/images/museum.svg"
+            isWide={isWide}
+          />
+          <CategoryButton
+            category="e mais..."
+            image="/images/earth.svg"
+            isWide={isWide}
+          />
+        </Stack>
+ */
